7aaaaf757d3c6905ca8a521ac48bbd8b71e04a30
unknown <binhhtse@gmail.com>
2023-01-29 21:13:44 +0700

    a

--- js/script.js
+++ js/script.js
@@ -73,39 +73,8 @@
     dots: false,
     arrows: false,
     autoplay: true,
-    autoplaySpeed: 3000,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 4,
-          slidesToScroll: 4,
-          infinite: true,
-          dots: true,
-        },
-      },
-      {
-        breakpoint: 900,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
+    autoplaySpeed: 3000
+    
   });
 
   $(".portfolio-gallery").slick({
