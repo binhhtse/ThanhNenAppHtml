476efb01b912b07c74af8fdd13fd698eea260974
BinhHT <binhhtse@gmail.com>
2023-01-30 14:47:53 +0700

    a

--- js/script.js
+++ js/script.js
@@ -5,165 +5,173 @@
  * GITHUB: https://github.com/themefisher/
  */
 
-(function ($) {
-  "use strict";
-  $(".video-play").modalVideo();
+(function($) {
+    "use strict";
+    $(".video-play").modalVideo();
 
-  $(".portfolio-single-slider").slick({
-    infinite: true,
-    arrows: false,
-    autoplay: true,
-    autoplaySpeed: 2000,
-  });
+    $(".portfolio-single-slider").slick({
+        infinite: true,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
 
-  $(".clients-logo").slick({
-    infinite: true,
-    arrows: false,
-    autoplay: true,
-    autoplaySpeed: 2000,
-  });
+    $(".clients-logo").slick({
+        infinite: true,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
 
-  $(".testimonial-wrap").slick({
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    infinite: true,
-    dots: false,
-    arrows: false,
-    autoplay: true,
-    autoplaySpeed: 6000,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 4,
-          slidesToScroll: 4,
-          infinite: true,
-          dots: true,
-        },
-      },
-      {
-        breakpoint: 900,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  });
-   
+    $(".testimonial-wrap").slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        infinite: true,
+        dots: false,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 6000,
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 4,
+                    infinite: true,
+                    dots: true,
+                },
+            },
+            {
+                breakpoint: 900,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                },
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
+    });
 
-  $(".banner-gallery").slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    infinite: true,
-    dots: false,
-    arrows: false,
-    autoplay: true,
-    autoplaySpeed: 3000
-    
-  });
 
-  $(".portfolio-gallery").slick({
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    infinite: true,
-    dots: false,
-    arrows: false,
-    autoplay: true,
-    autoplaySpeed: 6000,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 4,
-          slidesToScroll: 4,
-          infinite: true,
-          dots: true,
-        },
-      },
-      {
-        breakpoint: 900,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  });
+    $(".banner-gallery").slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false,
+        arrows: false,
+        autoplay: false,
+        autoplaySpeed: 3000
 
-  $(".portfolio-gallery").each(function () {
-    $(this)
-      .find(".popup-gallery")
-      .magnificPopup({
-        type: "image",
-        gallery: {
-          enabled: true,
-        },
-      });
-  });
-  
-  $(".banner-gallery").each(function () {
-    $(this)
-      .find(".popup-gallery")
-      .magnificPopup({
-        type: "image",
-        gallery: {
-          enabled: true,
-        },
-      });
-  });
-  var map;
+    });
 
-  function initialize() {
-    var mapOptions = {
-      zoom: 13,
-      center: new google.maps.LatLng(50.97797382271958, -114.107718560791),
-      // styles: style_array_here
-    };
-    map = new google.maps.Map(
-      document.getElementById("map-canvas"),
-      mapOptions
-    );
-  }
+    $(".portfolio-gallery").slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        infinite: true,
+        dots: false,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 6000,
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 4,
+                    infinite: true,
+                    dots: true,
+                },
+            },
+            {
+                breakpoint: 900,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                },
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
+    });
 
-  var google_map_canvas = $("#map-canvas");
+    $(".portfolio-gallery").each(function() {
+        $(this)
+            .find(".popup-gallery")
+            .magnificPopup({
+                type: "image",
+                gallery: {
+                    enabled: true,
+                },
+            });
+    });
 
-  if (google_map_canvas.length) {
-    google.maps.event.addDomListener(window, "load", initialize);
-  }
+    $(".banner-gallery").each(function() {
+        $(this)
+            .find(".popup-gallery")
+            .magnificPopup({
+                type: "image",
+                gallery: {
+                    enabled: true,
+                },
+            });
+    });
+    var map;
 
-  // Counter
+    function initialize() {
+        var mapOptions = {
+            zoom: 13,
+            center: new google.maps.LatLng(50.97797382271958, -114.107718560791),
+            // styles: style_array_here
+        };
+        map = new google.maps.Map(
+            document.getElementById("map-canvas"),
+            mapOptions
+        );
+    }
 
-  $(".counter-stat").counterUp({
-    delay: 10,
-    time: 1000,
-  });
+    var google_map_canvas = $("#map-canvas");
+
+    if (google_map_canvas.length) {
+        google.maps.event.addDomListener(window, "load", initialize);
+    }
+
+    // Counter
+
+    $(".counter-stat").counterUp({
+        delay: 10,
+        time: 1000,
+    });
+
+
+    $(".product-colors span").click(function() {
+        $(".product-colors span").removeClass("active");
+        $(this).addClass("active");
+        //$("body").css("background", $(this).attr("data-color"));
+        $(".product-price").css("color", $(this).attr("data-color"));
+        $(".product-button").css("color", $(this).attr("data-color"));
+        $(".product-pic").css("background-image", $(this).attr("data-pic"));
+    });
 })(jQuery);
