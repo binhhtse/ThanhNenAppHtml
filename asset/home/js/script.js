fcbf9444cb255e59b54765e2e5055cbb8a9fac11
BinhHT <binhhtse@gmail.com>
2023-02-03 09:28:02 +0700

    a

--- asset/home/js/script.js
+++ asset/home/js/script.js
@@ -114,7 +114,49 @@
         autoplaySpeed: 3000
 
     });
-
+    $(".news-gallery").slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        infinite: true,
+        dots: true,
+        dotsClass: 'slick-dots',
+        arrows: true,
+        autoplay: true,
+        prevArrow: '<div class="slick-prev"></div>',
+        nextArrow: '<div class="slick-next"></div>',
+        autoplaySpeed: 1200,
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 4,
+                    infinite: true,
+                    dots: true,
+                },
+            },
+            {
+                breakpoint: 900,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                },
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
+    });
     $(".portfolio-gallery").slick({
         slidesToShow: 6,
         slidesToScroll: 6,
